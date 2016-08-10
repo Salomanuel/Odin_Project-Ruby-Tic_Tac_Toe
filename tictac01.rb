0d66290f9d5df1644acd4aaf0173f0536353e1b5
Salomanuel <salomanue.aux+code@gmail.com>
2016-08-10 10:52:15 +0200

    initialized repo

---
+++ tictac01.rb
@@ -0,0 +1,50 @@
+module Board
+	def initialize
+		board_creator
+		show_board
+		#@@board[2][2] = "O"	
+	end
+	
+	def show_board
+		@@board.each { |j| puts j.join(" ") }
+	end
+
+	private
+	def board_creator
+		@@board = [[" ","A","B","C"]]
+		3.times do |j|
+			row = [j+1]
+			3.times { row << "." }
+			@@board << row
+		end
+	end
+end
+
+class Tictac
+	#implement players
+	include Board
+	def initialize
+		super
+		@move_count = 0
+	end
+
+	def move(x,y)
+		@move_count += 1
+		top_row = ("A".."C").to_a
+		puts "\nturn #{@move_count}"
+		puts "your move is #{top_row[x-1]}:#{y}"
+		if @@board[x][y] == "."
+			@@board[x][y] = "O"
+		else
+			puts "\nyou cannot do this move\n\n"
+		end
+		show_board
+	end
+
+	def score
+	end
+end
+
+uno = Tictac.new
+uno.move(2,2)
+uno.move(2,2)
